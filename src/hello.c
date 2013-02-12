afe1dd37684a9e23d5dd2eb844a598bc373a9123
bluekulu <bluekulu@bluekulu-R59P-R60P-R61P.(none)>
2013-02-12 19:18:59 +0000

    changed to c

---
+++ src/hello.c
@@ -0,0 +1,13 @@
+/****************
+* Helloworld.c
+* The most simplistic C program ever written.
+* An epileptic monkey on crack could write this code.
+*****************/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hell! O' world, why won't my code compile?\n\n");
+	return 0;
+}
+
